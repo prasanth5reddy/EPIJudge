ef4ede982529be5684685a980f57455f1f149812
Prasanth Buddareddygari <prasanthb588@gmail.com>
2019-09-24 19:20:40 -0700

    submitted parity.py

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,15 +1,30 @@
 from test_framework import generic_test
 
+lookup_table = []
+for i in range(2 ** 16):
+    p = 0
+    while i:
+        p ^= 1
+        # this will erase the lowest set bit
+        i &= i - 1
+    lookup_table.append(p)
+
 
 def parity(x):
     # TODO - you fill in here.
 
-    # O(n)
+    # # O(n)
     # c = 0
     # while x:
     #     c ^= x & 1
     #     x >>= 1
     # return c
+
+    # O(n/L) using look up table
+    # mask_size = 16
+    # bit_mask = 0xFFFF
+    # return lookup_table[x >> (3 * mask_size)] ^ lookup_table[(x >> (2 * mask_size)) & bit_mask] ^ lookup_table[
+    #     (x >> mask_size) & bit_mask] ^ lookup_table[x & bit_mask]
 
     # O(log n)
     x ^= x >> 32
