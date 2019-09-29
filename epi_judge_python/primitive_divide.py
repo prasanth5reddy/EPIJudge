2cbfc021215fcbccf8be811e11ca6f9831bc285c
Prasanth Buddareddygari <prasanthb588@gmail.com>
2019-09-28 19:11:56 -0700

    submitted primitive_divide.py

--- epi_judge_python/primitive_divide.py
+++ epi_judge_python/primitive_divide.py
@@ -3,7 +3,15 @@ from test_framework import generic_test
 
 def divide(x, y):
     # TODO - you fill in here.
-    return 0
+    ans, k = 0, 32
+    y_p = y << k
+    while x >= y:
+        while y_p > x:
+            y_p >>= 1
+            k -= 1
+        ans += 1 << k
+        x -= y_p
+    return ans
 
 
 if __name__ == '__main__':
