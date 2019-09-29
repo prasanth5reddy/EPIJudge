063215dbd03fe32ad34ea05b8081841c4e5e4208
Prasanth Buddareddygari <prasanthb588@gmail.com>
2019-09-28 20:10:37 -0700

    submitted power_x_y.py

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -3,7 +3,25 @@ from test_framework import generic_test
 
 def power(x, y):
     # TODO - you fill in here.
-    return 0.0
+    # ans = 1.0
+    # if y < 0:
+    #     y, x = -y, 1.0 / x
+    # while y:
+    #     if y & 1:
+    #         ans *= x
+    #     x, y = x * x, y >> 1
+    # return ans
+
+    # recursion
+    if y == 0:
+        return 1.0
+    if y == 1:
+        return x
+    if y < 0:
+        return pow(1.0 / x, -y)
+    else:
+        next_pow = pow(x * x, y >> 1)
+        return x * next_pow if y & 1 else next_pow
 
 
 if __name__ == '__main__':
