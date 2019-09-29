30fa2f2348968df0053fa07ed5473f106780b96d
Prasanth Buddareddygari <prasanthb588@gmail.com>
2019-09-28 18:37:26 -0700

    submitted primitive_multiply.py

--- epi_judge_python/primitive_multiply.py
+++ epi_judge_python/primitive_multiply.py
@@ -3,7 +3,26 @@ from test_framework import generic_test
 
 def multiply(x, y):
     # TODO - you fill in here.
-    return 0
+    # adding two numbers without arithmetic
+    def add(a, b):
+        while b:
+            c = a & b
+            a, b = a ^ b, c << 1
+        return a
+
+    # # adding two numbers without arithmetic - recursion
+    # def add(a, b):
+    #     if b:
+    #         return add(a ^ b, (a & b) << 1)
+    #     return a
+
+    ans = 0
+    while x:
+        if x & 1:
+            ans = add(ans, y)
+        x = x >> 1
+        y = y << 1
+    return ans
 
 
 if __name__ == '__main__':
