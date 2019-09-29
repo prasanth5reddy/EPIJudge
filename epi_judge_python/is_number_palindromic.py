bc261cece4d42bba5e156f82607c6d2847ec18e0
Prasanth Buddareddygari <prasanthb588@gmail.com>
2019-09-29 12:10:08 -0700

    submitted is_number_palindromic.py

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,8 +1,29 @@
 from test_framework import generic_test
+import math
+from reverse_digits import reverse
 
 
 def is_palindrome_number(x):
     # TODO - you fill in here.
+    # # using string manipulation
+    # return str(x) == str(x)[::-1]
+
+    # # using integer reverse
+    # if x < 0:
+    #     return False
+    # return x == reverse(x)
+
+    # checking lsd and msd
+    if x <= 0:
+        return x == 0
+    n_digits = math.floor(math.log10(x)) + 1
+    msd_mask = 10 ** (n_digits - 1)
+    for i in range(n_digits // 2):
+        if x // msd_mask != x % 10:
+            return False
+        x %= msd_mask
+        x //= 10
+        msd_mask //= 100
     return True
 
 
