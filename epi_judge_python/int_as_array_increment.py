f56d0192b544091bdd4eb9a686b618ec9d01993d
Prasanth Buddareddygari <prasanthb588@gmail.com>
2019-10-20 13:10:50 -0700

    submitted int_as_array_increment.py

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -3,7 +3,25 @@ from test_framework import generic_test
 
 def plus_one(A):
     # TODO - you fill in here.
-    return []
+    # # one line. But takes time and integer overflow might happen
+    # return list(map(int, str(int(''.join(map(str, A))) + 1)))
+
+    # Adding digit by digit
+    # Time complexity : O(n)
+    # Space complexity : O(1)
+    c = 1
+    for i in reversed(range(len(A))):
+        A[i] += c
+        if A[i] == 10:
+            A[i], c = 0, 1
+        else:
+            c = 0
+            break
+    if c:
+        # A.insert(0, c)
+        A[0] = 1
+        A.append(0)
+    return A
 
 
 if __name__ == '__main__':
