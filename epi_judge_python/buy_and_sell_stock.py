ae7c0825f6511310c4ab427dba43bd04a51a27f5
Prasanth Buddareddygari <prasanthb588@gmail.com>
2019-10-25 22:23:27 -0700

    submitted buy_and_sell_stock.py

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -2,8 +2,23 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices):
-    # TODO - you fill in here.
-    return 0.0
+    # Time complexity : O(n)
+    # Space complexity : O(1)
+    min_price, max_profit = float('inf'), float('-inf')
+    for price in prices:
+        min_price = min(min_price, price)
+        max_profit = max(max_profit, price - min_price)
+    return max_profit
+
+    # # DP solution using Kadane's algorithm of finding maximum subarray
+    # # find maximum subarray of
+    # # Time complexity : O(n)
+    # # Space complexity : O(1)
+    # max_cur, max_so_far = 0, 0
+    # for i in range(1, len(prices)):
+    #     max_cur = max(0, max_cur + prices[i] - prices[i - 1])
+    #     max_so_far = max(max_cur, max_so_far)
+    # return max_so_far
 
 
 if __name__ == '__main__':
