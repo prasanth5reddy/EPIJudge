530943aceac00f185042e2f965bb93378a66cba9
Prasanth Buddareddygari <prasanthb588@gmail.com>
2019-10-30 16:31:41 -0700

    submitted search_first_key.py

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -2,8 +2,20 @@ from test_framework import generic_test
 
 
 def search_first_of_k(A, k):
-    # TODO - you fill in here.
-    return 0
+    # Time complexity : O(log n)
+    # space complexity : O(1)
+    i, j = 0, len(A) - 1
+    ans = -1
+    while i <= j:
+        m = i + (j - i) // 2
+        if A[m] < k:
+            i = m + 1
+        elif A[m] == k:
+            ans = m
+            j = m - 1
+        else:
+            j = m - 1
+    return ans
 
 
 if __name__ == '__main__':
