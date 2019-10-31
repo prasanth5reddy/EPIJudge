ace7ef75810ca2e3f669ad6bc23e9f55132cae07
Prasanth Buddareddygari <prasanthb588@gmail.com>
2019-10-30 17:23:46 -0700

    submitted is_anonymous_letter_constructible.py

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,9 +1,27 @@
 from test_framework import generic_test
+import collections
 
 
 def is_letter_constructible_from_magazine(letter_text, magazine_text):
-    # TODO - you fill in here.
-    return True
+    # Time complexity : O(m + n) where m and n are number of characters in letter and magazine respectively
+    # Space complexity : O(L) where L is number of distinct characters in letter_text
+
+    # using dict
+    letter_counts = {}
+    for letter in letter_text:
+        letter_counts[letter] = letter_counts.get(letter, 0) + 1
+    for c in magazine_text:
+        if not letter_counts:
+            return True
+        if c in letter_counts:
+            letter_counts[c] -= 1
+            if letter_counts[c] == 0:
+                del letter_counts[c]
+
+    return not letter_counts
+
+    # # using collections.Counter
+    # return not (collections.Counter(letter_text) - collections.Counter(magazine_text))
 
 
 if __name__ == '__main__':
