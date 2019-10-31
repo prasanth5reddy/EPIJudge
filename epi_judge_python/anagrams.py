09ca0d5e994e692057d1d4099b612a5d982ea1e5
Prasanth Buddareddygari <prasanthb588@gmail.com>
2019-10-30 17:01:44 -0700

    submitted anagrams.py

--- epi_judge_python/anagrams.py
+++ epi_judge_python/anagrams.py
@@ -2,8 +2,17 @@ from test_framework import generic_test, test_utils
 
 
 def find_anagrams(dictionary):
-    # TODO - you fill in here.
-    return []
+    # Time complexity : O(n m log m) where n is number of strings and m is the max length of a string
+    # space complexity : O(n m)
+    sorted_dict = {}
+    for string in dictionary:
+        sorted_key = ''.join(sorted(string))
+        if sorted_key in sorted_dict:
+            sorted_dict[sorted_key] += [string]
+        else:
+            sorted_dict[sorted_key] = [string]
+
+    return [group for group in sorted_dict.values() if len(group) >= 2]
 
 
 if __name__ == '__main__':
