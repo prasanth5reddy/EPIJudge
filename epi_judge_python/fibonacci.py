39100e9dbda5050a0d421cee10ca3c2d647a25a7
Prasanth Buddareddygari <prasanthb588@gmail.com>
2019-11-17 14:46:49 -0700

    submitted fibonacci.py

--- epi_judge_python/fibonacci.py
+++ epi_judge_python/fibonacci.py
@@ -3,7 +3,14 @@ from test_framework import generic_test
 
 def fibonacci(n):
     # TODO - you fill in here.
-    return -1
+    if n == 0 or n == 1:
+        return n
+    a, b = 0, 1
+    for i in range(2, n + 1):
+        c = a + b
+        a = b
+        b = c
+    return b
 
 
 if __name__ == '__main__':
