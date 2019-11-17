e14a239e81cfccd4c2cfae64f0d352a96b8a1dd0
Prasanth Buddareddygari <prasanthb588@gmail.com>
2019-11-17 15:49:41 -0700

    submitted number_of_score_combinations.py

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -2,8 +2,16 @@ from test_framework import generic_test
 
 
 def num_combinations_for_final_score(final_score, individual_play_scores):
-    # TODO - you fill in here.
-    return 0
+    # DP : let A[i][j] be the number of combinations with scores s[0]...s[i] that result in final score of j
+    # Time complexity = O(sn) where n = final_score and s = len(individual_play_scores)
+    # Space complexity = O(sn)
+    dp = [[1] + [0] * final_score] * len(individual_play_scores)
+    for i in range(len(individual_play_scores)):
+        for j in range(1, final_score + 1):
+            w_out = dp[i - 1][j] if i >= 0 else 0
+            w = dp[i][j - individual_play_scores[i]] if j >= individual_play_scores[i] else 0
+            dp[i][j] = w_out + w
+    return dp[-1][-1]
 
 
 if __name__ == '__main__':
