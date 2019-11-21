c77b0cb337d2d7a323848748b4774d66268dd327
Prasanth Buddareddygari <prasanthb588@gmail.com>
2019-11-20 19:05:47 -0700

    submitted two_sum.py

--- epi_judge_python/two_sum.py
+++ epi_judge_python/two_sum.py
@@ -2,8 +2,18 @@ from test_framework import generic_test
 
 
 def has_two_sum(A, t):
-    # TODO - you fill in here.
-    return True
+    # Time complexity = O(n)
+    # Space complexity = O(1)
+    i, j = 0, len(A) - 1
+    while i <= j:
+        s = A[i] + A[j]
+        if s == t:
+            return True
+        elif s < t:
+            i += 1
+        else:
+            j -= 1
+    return False
 
 
 if __name__ == '__main__':
