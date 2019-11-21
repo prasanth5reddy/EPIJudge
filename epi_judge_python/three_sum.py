fa74d022c6d443f6a65def4e377511ba6fab34dd
Prasanth Buddareddygari <prasanthb588@gmail.com>
2019-11-20 19:05:54 -0700

    submitted three_sum.py

--- epi_judge_python/three_sum.py
+++ epi_judge_python/three_sum.py
@@ -2,8 +2,20 @@ from test_framework import generic_test
 
 
 def has_three_sum(A, t):
-    # TODO - you fill in here.
-    return True
+    # Time complexity = O(n^2)
+    # Space complexity = O(1)
+    A.sort()
+    for i in range(len(A)):
+        j, k = i, len(A) - 1
+        while j <= k:
+            s = A[i] + A[j] + A[k]
+            if s == t:
+                return True
+            elif s < t:
+                j += 1
+            else:
+                k -= 1
+    return False
 
 
 if __name__ == '__main__':
