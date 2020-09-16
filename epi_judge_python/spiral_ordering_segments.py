81fcc1be6d3b3e4b0c2f9f9d7a0e5229778275fd
Prasanth Buddareddygari <prasanthb588@gmail.com>
2020-09-15 23:14:21 -0700

    solved spiral_ordering_segments.py

--- epi_judge_python/spiral_ordering_segments.py
+++ epi_judge_python/spiral_ordering_segments.py
@@ -2,8 +2,22 @@ from test_framework import generic_test
 
 
 def matrix_in_spiral_order(square_matrix):
-    # TODO - you fill in here.
-    return []
+    if len(square_matrix) == 0:
+        return []
+    if len(square_matrix) == 1:
+        return square_matrix[0]
+    ans = []
+    x, y, d = 0, 0, 0
+    direction = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    for _ in range(len(square_matrix) * len(square_matrix[0])):
+        ans.append(square_matrix[x][y])
+        square_matrix[x][y] = None
+        nx, ny = x + direction[d][0], y + direction[d][1]
+        if not (0 <= nx < len(square_matrix) and 0 <= ny < len(square_matrix[0]) and square_matrix[nx][ny] is not None):
+            d = (d + 1) % 4
+            nx, ny = x + direction[d][0], y + direction[d][1]
+        x, y = nx, ny
+    return ans
 
 
 if __name__ == '__main__':
